7dcc23ecd1b0cb490484ed256d71014a53c640f4
MinaHany256 <minahanyfoad@gmail.com>
2024-07-29 14:01:40 +0300

    Add project files.

---
+++ Assignment Collection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
