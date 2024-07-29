f8cf64e82dda4c15203a502f396022e5578faad5
MinaHany256 <minahanyfoad@gmail.com>
2024-07-29 14:06:19 +0300

    Part 02 NonGeneric Collections - ArrayList

--- Demo Collection/Program.cs
+++ Demo Collection/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace Demo_Collection
+﻿
+using System.Collections;
+
+//using System.Collections.Generic;
+
+namespace Demo_Collection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Non-Generic Collections - ArrayList
+
+            //ArrayList Numbers = new ArrayList();
+
+            //Console.WriteLine($"Count = {Numbers.Count} , Capacity = {Numbers.Capacity}");
+
+            //Numbers.Add(1);
+            //Numbers.Add(2);
+            //Numbers.AddRange(new int[] {3,4});
+
+            ////ArrayList NumbersX = new ArrayList(5);  // Capacity = 5  
+
+
+            //Numbers.Add(5);
+
+            //Console.WriteLine($"Count = {Numbers.Count} , Capacity = {Numbers.Capacity}");
+
+
+            //Numbers.TrimToSize();  // Free | Delete | Release Unused Bytes
+
+            //Console.WriteLine("---- After Trimming ----");
+            //Console.WriteLine($"Count = {Numbers.Count} , Capacity = {Numbers.Capacity}");
+
+
+            //ArrayList Numbers = new ArrayList() { 1, 2, 3 };
+
+            //Numbers.Add(1);
+            //Numbers.Add(2);
+            //Numbers.Add(3);
+
+            //ArrayList Numbers02 = new ArrayList(4) { 1, 2, 3, 4 };
+            //ArrayList Numbers = new ArrayList(Numbers02); // Second Paramter
+
+            //ArrayList Numbers = new ArrayList(5) { 1, 2, 3, 4, 5};
+
+            //Console.WriteLine($"Count = {Numbers.Count} , Capacity = {Numbers.Capacity}");
+
+
+            //for (int i = 6; i <= 21; i++)
+            //{
+            //    Numbers.Add(i);
+            //}
+
+            //Console.WriteLine($"Count = {Numbers.Count} , Capacity = {Numbers.Capacity}");
+
+
+            #endregion
         }
     }
 }
